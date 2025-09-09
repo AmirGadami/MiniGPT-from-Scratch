37a8f36f4a15a5ccf1e9fc13d04ed6ca6ed5a022
AmirGadami <aghad051@uottawa.ca>
2025-09-09 17:16:42 -0400

    MultiHead Attention is added

--- model.py
+++ model.py
@@ -38,3 +38,93 @@ class PositionalEncodings(nn.Module):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
     
+
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps:float=10**-6) ->None:
+        super().__init__()
+
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.ones(1))
+
+    def forward(self,x):
+        mean = x.mean(-1,keepdim=True)
+        std = x.std(-1,keepdim=True)
+        return self.alpha * (x- mean)/(std+ self.eps) +self.bias
+    
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model:int, d_ff:int, dropout:float):
+        super().__init__()
+
+        self.linear_1 = nn.Linear(d_model,d_ff)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff,d_model)
+
+    def forward(self, x):
+        x = self.linear_1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        return self.linear_2(x)
+    
+
+
+
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model:int, h:int , dropout:float):
+        super().__init__()
+
+        self.d_model = d_model
+        self.h = h
+
+        assert d_model % h == 0, 'd_model is not divisible by h'
+
+        self.d_k = self.d_model // h
+        self.w_q = nn.Linear(d_model,d_model)
+        self.w_v = nn.Linear(d_model,d_model)
+        self.w_k = nn.Linear(d_model,d_model)
+
+        self.w_o = nn.Linear(d_model,d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k=query.shape[-1]
+
+        attention_score = (query @ key.transpose(-1,-2))/math.sqrt(d_k)
+
+        if mask is not None:
+            attention_score = attention_score.masked_fill(mask == 0 , -1e9)
+        attention_score = attention_score.softmax(dim=-1)
+
+        if dropout is not None:
+            attention_score = dropout(attention_score)
+        
+        return (attention_score @value) , attention_score
+    
+
+
+    def forward(self, q,k,v, mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0], query.hsape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1 ,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1 ,2)
+
+        x, self.attention_score = self.attention(query, key, value, mask, self.dropout)
+
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        return self.w_o(x)
+
+
+
+
