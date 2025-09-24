3247538e2a8b746a191d0ff8575106b760b977b8
AmirGadami <aghad051@uottawa.ca>
2025-09-24 18:02:24 -0400

    validation loop is added

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
-from dataset import BilingualDataset
+from dataset import BilingualDataset, causal_mask
 from model import build_transformer
 from datasets import load_dataset
 from tokenizers import Tokenizer 
@@ -14,6 +14,88 @@ from tqdm import tqdm
 import warnings
 
 from pathlib import Path
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+
+        # build mask for target
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+
+        # calculate output
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+        # get next token
+        prob = model.project(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat(
+            [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
+        )
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
+
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, num_examples=2):
+    model.eval()
+    count = 0
+
+    source_texts = []
+    expected = []
+    predicted = []
+
+    try:
+        # get the console window width
+        with os.popen('stty size', 'r') as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        # If we can't get the console width, use 80 as default
+        console_width = 80
+
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch["encoder_input"].to(device) # (b, seq_len)
+            encoder_mask = batch["encoder_mask"].to(device) # (b, 1, 1, seq_len)
+
+            # check that the batch size is 1
+            assert encoder_input.size(
+                0) == 1, "Batch size must be 1 for validation"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch["src_text"][0]
+            target_text = batch["tgt_text"][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+            
+            # Print the source, target and model output
+            print_msg('-'*console_width)
+            print_msg(f"{f'SOURCE: ':>12}{source_text}")
+            print_msg(f"{f'TARGET: ':>12}{target_text}")
+            print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+
+            if count == num_examples:
+                print_msg('-'*console_width)
+                break
+
+
+
+
+
 
 def get_all_sentences(ds,lang):
     for item in ds:
@@ -128,6 +210,10 @@ def train_model(config):
             optimizer.zero_grad()
 
             global_step+=1
+        
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg),num_examples=2)
+
+        
 
         model_filename = get_weights_file_path(config, f'{epoch:02d}')
         torch.save({
@@ -141,3 +227,35 @@ if __name__ == "__main__":
     warnings.filterwarnings('ignore')
     config = get_config()
     train_model(config)
+    # # Load tokenizers
+    # tokenizer_src = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_src']))))
+    # tokenizer_tgt = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_tgt']))))
+
+    # # Load validation dataset
+    # _, val_dataloader, _, _ = get_ds(config)
+
+    # # Load model
+    # model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size())
+
+    # model_filename = get_weights_file_path(config, "04")   # change "00" to another epoch if needed
+    # state = torch.load(model_filename, map_location="cpu")
+
+    # model.load_state_dict(state['model_state_dict'])
+    # model.eval()
+
+    # device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    # model = model.to(device)
+
+    # # Run validation
+    # run_validation(
+    #     model,
+    #     val_dataloader,
+    #     tokenizer_src,
+    #     tokenizer_tgt,
+    #     max_len=50,
+    #     device=device,
+    #     print_msg=print,
+    #     num_examples=2
+    # )
+
+    
